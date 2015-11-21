89b9fbe78e082485395870bdcc969ce42a19945a
Saleem Ahmad <saleem.xavier@gmail.com>
2015-11-22 03:46:58 +0530

    Updated Exception handling code

--- gui.py
+++ gui.py
@@ -2,18 +2,34 @@ from Tkinter import *
 import socket
 
 s = socket.socket()
-s.connect(('192.168.7.2', 1234))
-def send_signal():
-	s.send('Light On')
+try:
+	s.connect(('192.168.7.2', 1234))   #Connects bealgebone on 1234 Port
+except socket.error as e:
+	print e
+
+def light_on():
+	try:
+		s.send('Light On')
+	except socket.error as e:
+		print e
 
 def light_off():
-	s.send('Light Off')
+	try:
+		s.send('Light Off')
+	except socket.error as e:
+		print e
 
-root = Tk()
-B = Button(root, text='Light On', command=send_signal)
-Off = Button(root, text = 'Light Off', command = light_off)
-B.pack()
-Off.pack()
+root = Tk()  	
+
+Button_on = Button(root, text='Light On', command=light_on)
+Button_off = Button(root, text = 'Light Off', command = light_off)
+Button_on.pack()
+Button_off.pack()
+
 root.mainloop()
-s.send('Close')
+try:
+	s.send('Close')
+except socket.error as e:
+	print e
+
 s.close()
